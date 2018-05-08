06b933c814c17f123bb38fbacfaa27fdb7530beb
Christopher McCoy <35251302+cmccoy13@users.noreply.github.com>
2018-05-08 15:43:45 -0700

    Update Route.java
    Added constructor and necessary functions for updating route info

--- Route.java
+++ Route.java
@@ -2,5 +2,26 @@
 
 public class Route
 {
+   private int duration;
+   private int basePrice;
+   private String origin;
+   private String destination;
    
+   public Route(int dur, int price, String origin, String destination)
+   {
+      this.duration = duration;
+      this.price = price;
+      this.origin = origin;
+      this.destination = destination;
+   }
+   
+   public void ChangeDuration(int newDuration)
+   {
+      duration = newDuration;
+   }
+   
+   public void ChangePrice(int newBasePrice)
+   {
+      basePrice = newBasePrice;
+   }
 }
