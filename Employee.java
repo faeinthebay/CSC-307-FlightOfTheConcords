981e785810a7699dd578093760dcd41acdbe5dc6
Andrew LD <offers@andrewiam.com>
2018-05-08 15:56:11 -0700

    Add outlines of classes for users

---
+++ Employee.java
@@ -0,0 +1,3 @@
+public static Employee extends User {
+	
+}
---
+++ User.java
@@ -0,0 +1,8 @@
+public class User(){
+	String username;
+	String hashedPassword; // TODO: how to store password hash?
+	String firstname;
+	String lastname;
+	String phoneNumber;
+	Date birthdate;
+}
