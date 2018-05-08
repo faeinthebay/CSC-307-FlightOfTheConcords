e41941b1455c1a467b30244d6437e71bd0c38b53
ayanpatel <satellite3@sbcglobal.net>
2018-05-08 15:55:09 -0700

    Created Flight and Price calculation classes

---
+++ Calculate_Price.java
@@ -0,0 +1,9 @@
+public class Calculate_Price {
+	
+	public static float calculate(Route route, int emptySeats) {
+		float price = route.getBasePrice();
+		//for all flights in route, use empty seats to figure out price
+		//base price in route info
+		return price;
+	}
+}
