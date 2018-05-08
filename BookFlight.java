675d6eee07cd796b658fc331e66082e32edcc460
Christopher McCoy <35251302+cmccoy13@users.noreply.github.com>
2018-05-08 16:00:44 -0700

    Update BookFlight.java

--- BookFlight.java
+++ BookFlight.java
@@ -1,4 +1,19 @@
 public class BookFlight
 {
-
+  private Flight flight;
+  private int numSeats;
+  
+  public BookFlight(Flight flight, int numSeats)
+  {
+    this.flight = flight;
+    this.numSeats = numSeats;
+  }
+  
+  /*I don't know if we need this class, so I am holding off to edit more*/
+  
+  public int generateConfirmationNumber()
+  {
+    //Create random confirmation number
+    //Needs to be saved in a booking
+  }
 }
